f24c617e7b15c0715f066d3c8e1da7622e08e624
samuel dahl <samueldahl@samuels-MacBook-Pro.local>
2020-10-20 11:24:55 -0600

    I have expanded helloworld.py to tell the world a little about myself.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
 print("Hello world!")
+print("As Samuel Dahl, I hereby reserve the right to improve my github skills to make myself employable in the future, so that I can some day provide bread and bicycles for my children.")
+print("I look forward to going to topaz mountain in the future, I think that I will rather enjoy it")
+print("Some day I need to get married, and that intimidates me. I need to overcome many personal vices before I will be relationship ready.")
+print("One significant vice is the vice of purposeless internet use. It is not invincible and can be overcome if I am persistant.")
+dank = 4
+memes = 5
+print("Dank = " + str(dank))
+print("Memes = " + str(memes))
+print("Dank + memes = " + str(dank + memes))
